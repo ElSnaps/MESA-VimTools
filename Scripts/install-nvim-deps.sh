c79f97447643036f69478ff0a2210fe69587335b
sunny <sunny@projectmesa.net>
2023-07-15 15:18:48 +0200

    fixes to installer perms

--- Scripts/install-nvim-deps.sh
+++ Scripts/install-nvim-deps.sh
@@ -9,9 +9,9 @@ sudo apt install curl -y
 # Install neovim (check if package manager supports min version)
 sudo apt install neovim -y
 wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz
-tar xzf nvim-linux64.tar.gz
-cp nvim-linux64/bin/nvim /bin/
-rm -rf nvim-linux64*
+sudo tar xzf nvim-linux64.tar.gz
+sudo cp nvim-linux64/bin/nvim /bin/
+sudo rm -rf nvim-linux64*
 
 # Install neovim plug.
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
@@ -25,11 +25,11 @@ wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 sudo apt-get install -f
 rg --version
-rm -rf ripgrep_13.0.0_amd64*
+sudo rm -rf ripgrep_13.0.0_amd64*
 
 # Install neovim config
-mkdir -p ~/.config/nvim
-cp init.vim ~/.config/nvim
+sudo mkdir -p ~/.config/nvim
+sudo cp init.vim ~/.config/nvim
 
 # Install Plugins
 nvim +PlugInstall +qall
